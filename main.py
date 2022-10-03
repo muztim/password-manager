ec794766fced1966e8d1c4b5def068bfe5cea145
Timothee Muzaliwa <tmuzaliwwa@gmail.com>
2022-10-03 18:57:46 +0300

    feat: Search feature that returns website details if existant

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -25,18 +26,54 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if website == "" or password == "":
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
 
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \n"
-                                                              f"Password: {password} \nDo you want to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            # if file not found, create new file
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # updating the old data with the new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+# ----------------------------- SEARCH -------------------------------- #
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title=f"{website}", message="Data File not found")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=f"{website}", message=f"email: {email}\nPassword:{password}")
+        else:
+            messagebox.showinfo(title=f"{website}", message=f"No details for {website} exists.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -59,20 +96,22 @@ password_label = Label(text="Password:", background="white")
 password_label.grid(column=0, row=3)
 
 # entries
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry()
+website_entry.grid(row=1, column=1, sticky="EW")
 website_label.focus()
-email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "tmuzaliwwa@gmail.com")
-password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
+email_entry = Entry()
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.insert(0, "my@test.com")
+password_entry = Entry()
+password_entry.grid(row=3, column=1, sticky="EW")
 
 # buttons
 gen_password_button = Button(text="Generate Password", command=generate_password)
-gen_password_button.grid(column=2, row=3)
+gen_password_button.grid(row=3, column=2, sticky="EW")
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
+search_button = Button(text="Search", command=search)
+search_button.grid(row=1, column=2, sticky="EW")
 
 
 window.mainloop()
